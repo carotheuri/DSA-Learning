fe4ed4d671f8d06f99790cff684961a48ae9d226
carotheuri <princesstheush2@gmail.com>
2024-05-04 11:27:09 +0300

    adding delete value method on linkedlist

--- src/main/java/com/mycompany/app/datastructures/LinkedList.java
+++ src/main/java/com/mycompany/app/datastructures/LinkedList.java
@@ -1,8 +1,7 @@
 package com.mycompany.app.datastructures;
+public class LinkedList {
 
-public class LinkedList{
-
-    private static class Node{
+    private class Node {
         int data;
         Node next;
 
@@ -10,14 +9,67 @@ public class LinkedList{
             this.data = data;
         }
     }
-    public Node head;
-    public void addFront(int data){
+
+    private Node head;
+    int nodeCount = 0;
+
+    public void addFront(int data) {
         Node newNode = new Node(data);
-        if(head == null)
-            head = newNode;
-            return;
-        newNode.next = head;
-        head = newNode;
+        newNode.next = head; // Set newNode's next to the current head
+        head = newNode;      // Update head to newNode
+        nodeCount++;
+    }
+    public void addBack(int data) {
+        Node newNode = new Node(data);
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+        nodeCount++;
+    }
+    public int getFirstNode() {
+        if (head == null) {
+            throw new IllegalStateException("Empty List");
+        }
+        return head.data;
     }
 
+    public int getLastNode() {
+        if (head == null) {
+            throw new IllegalStateException("Empty List");
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.data;
+    }
+    public int size(){
+        return nodeCount;
+    }
+    public void clear(){
+        head = null;
+        nodeCount = 0;
+    }
+    public void deleteValue(int data){
+        if (head == null) {
+            throw new IllegalStateException("Empty List");
+        }
+        if(head.data == data) {
+            head = head.next;
+            nodeCount -=1;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if(current.next.data == data){
+                current.next = current.next.next;
+                nodeCount -=1;
+            }
+            else{
+                current = current.next;
+            }
+
+        }
+    }
 }
---
+++ src/test/java/com/mycompany/app/datastructures/LinkedListTest.java
@@ -0,0 +1,73 @@
+package com.mycompany.app.datastructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+    private LinkedList linkedList = new LinkedList();
+    @Test
+    public void addFront() {
+        linkedList.addFront(5);
+        linkedList.addFront(31);
+    }
+
+    @Test
+    public void getFistNode() {
+        linkedList.addFront(5);
+        linkedList.addFront(31);
+        Assert.assertEquals(31,linkedList.getFirstNode());
+    }
+    @Test
+    public void getLastNode() {
+        linkedList.addFront(10);
+        linkedList.addFront(31);
+        linkedList.addFront(48);
+        Assert.assertEquals(10,linkedList.getLastNode());
+    }
+
+
+    @Test
+    public void addBack() {
+        linkedList.addFront(10);
+        linkedList.addFront(31);
+        linkedList.addFront(48);
+        linkedList.addBack(52);
+        Assert.assertEquals(52,linkedList.getLastNode());
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(0,linkedList.size());
+        linkedList.addFront(10);
+        linkedList.addFront(31);
+        Assert.assertEquals(2,linkedList.size());
+        linkedList.addFront(48);
+        Assert.assertEquals(3,linkedList.size());
+        linkedList.addBack(52);
+        Assert.assertEquals(4,linkedList.size());
+    }
+
+    @Test
+    public void clear() {
+        linkedList.addFront(10);
+        linkedList.addFront(31);
+        linkedList.addFront(48);
+        linkedList.addBack(52);
+        Assert.assertEquals(4,linkedList.size());
+        linkedList.clear();
+        Assert.assertEquals(0,linkedList.size());
+    }
+    @Test
+    public void deleteValue() {
+        linkedList.addFront(10);
+        linkedList.addFront(31);
+        linkedList.addFront(48);
+        linkedList.addFront(52);
+        Assert.assertEquals(4,linkedList.size());
+        linkedList.deleteValue(48);
+        Assert.assertEquals(3,linkedList.size());
+        //Assert.assertEquals(31,linkedList.getLastNode());
+    }
+}
